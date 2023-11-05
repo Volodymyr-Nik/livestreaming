ca8243eec36133b2820fefad8500f9f632c5f86a
Volodymyr Nikitiuk <vmslex@gmail.com>
2023-11-05 18:32:57 -0500

    add all files

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-header />
+    <keep-alive>
+    <router-view></router-view>
+    </keep-alive>
+    <v-footer />
+</template>
+
+<script>
+import vHeader from "./components/v-header.vue";
+import vFooter from "./components/v-footer.vue";
+
+
+export default {
+  name: "App",
+  components: {
+    vHeader,
+    vFooter,
+  },
+};
+</script>
+
+<style>
+#app {
+  color: #fff;
+}
+</style>
